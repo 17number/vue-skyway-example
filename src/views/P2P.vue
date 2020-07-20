621e46831b791ccd28fe723c5f7fdbd20d661d8a
r17n <4716989+17number@users.noreply.github.com>
2020-07-20 10:07:47 +0900

    feat: select video and audio device

--- src/views/P2P.vue
+++ src/views/P2P.vue
@@ -4,18 +4,51 @@
     <h2>P2P (example)</h2>
     <div id="me">
       <h3>Source (You)</h3>
-      <video id="my-video" width="400px" autoplay muted playsinline></video>
+      <video
+        id="my-video"
+        width="400px"
+        autoplay
+        muted
+        playsinline
+        :srcObject.prop="stream"
+        v-if="stream"
+      />
+      <!-- 接続オプション -->
+      <div>
+        <label>Video</label>
+        <select v-model="selectedVideo" @change="resetVideo" v-if="videos.length > 0">
+          <option :value="null" selected>なし</option>
+          <option :value="video.deviceId" v-for="(video, i) in videos" :key="i">{{ video.label }}</option>
+        </select>
+      </div>
+      <div>
+        <label>Audio</label>
+        <select v-model="selectedAudio" @change="resetVideo" v-if="audios.length > 0">
+          <option :value="null" selected>なし</option>
+          <option :value="audio.deviceId" v-for="(audio, i) in audios" :key="i">{{ audio.label }}</option>
+        </select>
+      </div>
       <p>My Peer ID: {{ srcId }}</p>
     </div>
     <div id="dest">
+      <!-- 接続先 -->
       <div>
         <label for="dest-id">Destination Peer ID</label>
         <input type="text" id="dest-id" v-model="destId">
+      </div>
+      <div>
         <button id="make-call" @click="call" v-if="!mediaConnection">発信</button>
         <button id="close" @click="close" v-if="mediaConnection">切断</button>
       </div>
       <h3>Destination</h3>
-      <video id="dest-video" width="400px" autoplay playsinline></video>
+      <video
+        id="dest-video"
+        width="400px"
+        autoplay
+        playsinline
+        :srcObject.prop="remoteStream"
+        v-if="remoteStream"
+      />
     </div>
   </div>
 </template>
@@ -30,11 +63,21 @@ export default {
   data() {
     return {
       peer: null,
+      videos: [],
+      audios: [],
+      selectedVideo: null,
+      selectedAudio: null,
       stream: null,
+      remoteStream: null,
       srcId: null,
       destId: null,
       mediaConnection: null,
     };
+  },
+  async created() {
+    const devices = await navigator?.mediaDevices?.enumerateDevices() || [];
+    this.videos = devices.filter(device => device.kind === 'videoinput');
+    this.audios = devices.filter(device => device.kind === 'audioinput');
   },
   mounted() {
     this.initVideo();
@@ -42,23 +85,39 @@ export default {
   },
   methods: {
     async initVideo() {
+      const constraints = {
+        video: this.selectedVideo ? { deviceId: { exact: this.selectedVideo } } : false,
+        audio: this.selectedAudio ? { deviceId: { exact: this.selectedAudio } } : false,
+      };
       try {
-        this.stream = await navigator?.mediaDevices?.getUserMedia({video: true, audio: true})
+        this.stream = await navigator?.mediaDevices?.getUserMedia(constraints);
       } catch (error) {
         console.error('mediaDevice.getUserMedia() error:', error);
-        alert('ビデオの初期化に失敗しました。');
         return;
       }
 
       if (!this.stream) {
         console.error('mediaDevice.getUserMedia() failed:');
-        alert('ビデオの初期化に失敗しました。');
+        return;
+      }
+    },
+    closeVideo() {
+      if (!this.stream) {
+        console.log('no opening stream');
         return;
       }
 
-      const videoElm = document.getElementById('my-video')
-      videoElm.srcObject = this.stream;
-      videoElm.play();
+      const videoElm = document.getElementById('my-video');
+      if (videoElm?.srcObject) {
+        videoElm.srcObject.getTracks()?.forEach(track => track.stop());
+        videoElm.srcObject = null;
+        console.log('video stopped');
+      }
+      this.stream = null;
+    },
+    resetVideo() {
+      this.closeVideo();
+      this.initVideo();
     },
     initPeer() {
       this.peer = new Peer({
@@ -108,21 +167,25 @@ export default {
       // https://webrtc.ecl.ntt.com/api-reference/javascript.html#event-stream
       this.mediaConnection.on('stream', stream => {
         // video要素にカメラ映像をセットして再生
-        videoElm.srcObject = stream;
-        videoElm.play();
+        console.log({ on: 'rcv stream', stream });
+        this.remoteStream = stream;
       });
 
       // https://webrtc.ecl.ntt.com/api-reference/javascript.html#event-close-2
       this.mediaConnection.once('close', () => {
-        videoElm.srcObject.getTracks().forEach(track => track.stop());
-        videoElm.srcObject = null;
+        if (videoElm?.srcObject) {
+          videoElm.srcObject?.getTracks()?.forEach(track => track.stop());
+          videoElm.srcObject = null;
+        }
         this.mediaConnection = null;
+        this.remoteStream = null;
       });
     },
     close() {
       // https://webrtc.ecl.ntt.com/api-reference/javascript.html#close-forceclose
       this.mediaConnection?.close(true);
       this.mediaConnection = null;
+      this.remoteStream = null;
     },
   },
 }
